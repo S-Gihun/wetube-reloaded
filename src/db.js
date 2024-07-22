17bcd6dc99dfdbc3d80a36ef3e25df312b17675e
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2024-07-22 19:12:10 +0900

    initial commit

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube"); // database에 연결
+mongoose.connect(process.env.DB_URL); // database에 연결
 
 
 const db = mongoose.connection;
